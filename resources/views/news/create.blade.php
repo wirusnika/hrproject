4b88ee4f3f3ad15be87f78469ddb74157b8f808c
Alexander Pailodze <alexander.pailodze@gmail.com>
2019-07-04 15:05:12 +0400

    nik

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -4,7 +4,7 @@
     <form action="{{ route('news.store') }}" method="post">
         {{ csrf_field() }}
 
-        <div class="container calendar-create-event text-center">
+        <div class="container news-create-post text-center">
             <div class="event-header">
                 <div class="clouds">
                     <h2 class="text-center mt-4 font-weight-bold">Create New Post</h2>
@@ -20,33 +20,26 @@
                         </div>
                         <div class="col-md-12 mt-5">
                             <h3>Event Description</h3>
-                            <textarea class="event-description" name="description" type="text" placeholder=""></textarea>
+                            <textarea class="event-description" name="description" type="text"
+                                      placeholder=""></textarea>
                         </div>
                         <div class="col-md-12 mt-5">
                             <div class="-image">
                                 <img src="/img/iceberg.png" alt="">
                             </div>
-                            <input name="emailBroadcast" type="checkbox" class="mt-1">
-                            <label class="form-check-label" for="exampleCheck1">Check For Email Broadcast</label>
+                            <label class="check-for-mail">Check For Email Broadcast
+                                <input type="checkbox" checked="checked">
+                                <span class="check-mark"></span>
+                            </label>
+                            <hr style="border-color: #f8fafc">
+                            <button class="create" formaction="{{ route('news.create') }}">Create
+                            </button>
                         </div>
                     </div>
                     <div class="row">
-
                         <div class="col-md-3 text-center setting-logo">
                             <p>Powered By</p>
                             <img src="/img/cz.png" alt="">
-
-                            {{--<div class="col-md-12">--}}
-                            {{--<h2>Description</h2>--}}
-                            {{--<label>--}}
-                            {{--<textarea rows="5" cols="45" name="description" class="form-control-message" ></textarea>--}}
-                            {{--</label><br><br>--}}
-                            {{--<label>--}}
-                            {{--<input name="emailBroadcast" type="checkbox" class="">--}}
-                            {{--</label>--}}
-                            {{--<label class="form-check-label" for="exampleCheck1">Check For Email Broadcast</label>--}}
-
-                            {{--</div>--}}
                         </div>
                     </div>
                 </div>
